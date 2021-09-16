3dcd3da7d3c53a48b999fa6abf086ad24dd53b0d
cz.official <cz.official@MacBook-Pro-2017-CZ.local>
2021-09-16 18:06:35 +0200

    Starting all over

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,10 +1,1 @@
 
-variable "location" {}
-variable "resource_group" {}
-variable "application_type" {}
-
-variable "public_ip_address_id" {}
-variable "subnet_id" {}
-
-variable "vm_admin_username" {}
-variable "vm_password" {}
