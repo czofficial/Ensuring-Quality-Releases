090d26dfe758d5949897f8803f6dde9463eceb53
cz.official <cz.official@MacBook-Pro-2017-CZ.local>
2021-09-22 09:12:15 +0200

    Bugfix terraform

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -11,6 +11,16 @@ resource "azurerm_network_interface" "test" {
   }
 }
 
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+output "tls_private_key" { 
+    value = tls_private_key.example_ssh.private_key_pem 
+    sensitive = true
+}
+
 resource "azurerm_linux_virtual_machine" "test" {
   name                = "${var.prefix}-vm"
   location            = var.location
