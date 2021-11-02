3ee2f18be2506158b0e7424af235a34608a22b52
cz.official <cz.official@MacBook-Pro-2017-CZ.local>
2021-11-02 18:22:24 +0100

    Update tests

--- automatedtesting/selenium/test-suite.py
+++ automatedtesting/selenium/test-suite.py
@@ -7,7 +7,7 @@ def timestamp():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 # Start the browser and login with standard_user
-def login (driver, user, password):
+def login (driver, user="standard_user", password="secret_sauce"):
     #Login
     driver.get('https://www.saucedemo.com/')
     driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
@@ -16,37 +16,11 @@ def login (driver, user, password):
     assert 'https://www.saucedemo.com/inventory.html' in driver.current_url
     print(timestamp() +' Login successful with username '+ user + ' and password '+ password)
 
-def add_cart(driver,n):
-    acum = 0
-    for i in range(n):
-        element = "a[id='item_" + str(i) + "_title_link']"
-        driver.find_element_by_css_selector(element).click()
-        driver.find_element_by_css_selector("button.btn_primary.btn_inventory").click()
-        product = driver.find_element_by_css_selector("div[class='inventory_details_name large_size']").text
-        print(timestamp() + " " + product + " added to shopping cart!")
-        driver.find_element_by_css_selector("button.inventory_details_back_button").click()
-        acum +=1
-    print(timestamp() +' '+ str(acum) + ' items added to cart successfully.')
-
-def remove_cart(driver,n):
-    acum = 0
-    for i in range(n):
-        element = "a[id='item_" + str(i) + "_title_link']"
-        driver.find_element_by_css_selector(element).click()
-        driver.find_element_by_css_selector("button.btn_secondary.btn_inventory").click()
-        product = driver.find_element_by_css_selector("div[class='inventory_details_name large_size']").text
-        print(timestamp() + " " + product + " removed from shopping cart!")
-        driver.find_element_by_css_selector("button.inventory_details_back_button").click()
-        acum +=1
-    print(timestamp() +' '+ str(acum) + ' items removed from cart successfully.')
-
 if __name__ == "__main__":
     print ('Starting the browser...')
-    # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument("--headless") 
     driver = webdriver.Chrome(options=options)
-    #driver = webdriver.Chrome()
 
     print (timestamp()+' Browser started successfully. Navigating to the demo page to login.')
 
@@ -54,5 +28,4 @@ if __name__ == "__main__":
     add_cart(driver, 6)
     remove_cart(driver, 6)
 
-    print(timestamp() + ' Selenium Tests DONE')
-    
+    print(timestamp() + ' Selenium Test Completed')
